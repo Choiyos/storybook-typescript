ce38b70a7f5995bc7b9d82fe13612230b97234ef
Choiyos <choiyos@youngint.com>
2020-02-12 18:17:48 +0900

    Docs와 Description에 대한 기능 추가, Knobs 적용되도록 수정

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,20 @@
-import { addParameters } from '@storybook/react';
-import { DocsPage, DocsContainer } from '@storybook/addon-docs/blocks';
- 
+import { addParameters } from "@storybook/react";
+import { DocsPage, DocsContainer } from "@storybook/addon-docs/blocks";
+
 addParameters({
   docs: {
     container: DocsContainer,
-    page: DocsPage,
+    page: DocsPage
   },
+  options: {
+    /**
+     * display the top-level grouping as a "root" in the sidebar
+     * @type {Boolean}
+     */
+    showRoots: false,
+    storySort: (a, b) =>
+      a[1].kind === b[1].kind
+        ? 0
+        : a[1].id.localeCompare(b[1].id, undefined, { numeric: true })
+  }
 });
