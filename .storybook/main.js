c625574a3259ac1d51a919bbd7fb4e8c10a545a7
Choiyos <choiyos@youngint.com>
2020-02-12 18:38:57 +0900

    TypeScript로 변환

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,7 @@
 const createCompiler = require("@storybook/addon-docs/mdx-compiler-plugin");
 
 module.exports = {
-  stories: ["../src/**/*.stories.(js|mdx)"],
+  stories: ["../src/**/*.stories.(js|mdx|tsx)"],
   // 1. register the docs panel (as opposed to '@storybook/addon-docs' which
   //    will configure everything with a preset)
   addons: [
